54f81528ced4ea40c126dfffcf37fb3c9eeb98cb
Amazigh0895 <arab.fechetah@outlook.fr>
2024-09-03 09:29:40 +0200

    rajout du .png automatiquement dans le cas ou l'utilisatezur ne le precise pas

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ import qrcode
 import qrcode.constants
 
 
-def generate_qr_code(data, filename='qrcode.png'):
+def generate_qr_code(data, filename):
     """
     Fonction qui génére le QRCode
-    Arguments: 
+    Arguments:
      data(ca peut etre une chaine de caractére ou autre chose) :ca represente le contenue du QRCode
      filename(str) : le nom du fichier
     """
+    if not filename.lower().endswith(".png"):
+        filename += ".png"
+
     # on crée une instance de l'objet QRCode et on parametre les valeurs
     qr = qrcode.QRCode(
         version=1,
@@ -18,11 +21,11 @@ def generate_qr_code(data, filename='qrcode.png'):
     )
     qr.add_data(data)
     qr.make(fit=True)
-    
+
     img = qr.make_image(fill_color="black", back_color="white")
     img.save(filename)
     print(f" QR code généré et sauvegardé sous le nom {filename}")
 
 
 data = "https://www.seia-electronique.fr/"
-generate_qr_code(data,"scannermoi.png")
+generate_qr_code(data, "scannermoi.png")
