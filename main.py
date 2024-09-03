4ee251ce687a1aa898c4a669802676366c82d458
Amazigh0895 <arab.fechetah@outlook.fr>
2024-09-03 09:03:34 +0200

    ajout du fichier main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+import qrcode
+import qrcode.constants
+
+
+def generate_qr_code(data, filename='qrcode.png'):
+    """
+    Fonction qui génére le QRCode
+    Arguments: 
+     data(ca peut etre une chaine de caractére ou autre chose) :ca represente le contenue du QRCode
+     filename(str) : le nom du fichier
+    """
+    # on crée une instance de l'objet QRCode et on parametre les valeurs
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+    
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save(filename)
+    print(f" QR code généré et sauvegardé sous le nom {filename}")
+
+
+data = "https://www.seia-electronique.fr/"
+generate_qr_code(data,"scannermoi.png")
